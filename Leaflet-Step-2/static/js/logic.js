354b7052add6e770af326dc03a952f2b7a96053d
Yash5 <yashalphabetagamma@gmail.com>
2021-04-17 11:36:39 -0700

    finalllyyyyyyy

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,82 +1,80 @@
-// Map object centered at Buena Vista park
-var myMap = L.map("map", {
-    center: [37.7686550338169, -122.4408238032053],
-    zoom: 8
-});
+function createMap(earthquakes, tectonicplates) {
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    })
 
-  //Dark Mapbox background
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-}).addTo(myMap);
+    //Sat Layer
+    var satmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    })
 
+    //Outdoors Layer
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox.outdoors",
+        accessToken: API_KEY
+    })
 
-var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+    // map object at Buena Vista
+    var myMap = L.map("map", {
+        center: [37.7686550338169, -122.4408238032053],
+        zoom: 8,
+        layers: [darkmap, satmap, outdoors]
 
-d3.json(geoData, function (data) {
+    });
 
-function styleInfo(feature) {
-    return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.69
-    };
+    var basemaps = {
+        "Satellite Map": satmap,
+        "Dark Map": darkmap,
+        "Outdoors": outdoors,
     }
 
-function getColor(magnitude) {
-    switch (true) {
-        case magnitude > 5:
-            return "#ff0000";
-        case magnitude > 4:
-            return "#ff7300";
-        case magnitude > 3:
-            return "#fbff00";
-        case magnitude > 2:
-            return "#eecc00";
-        case magnitude > 1:
-            return "#d4ee00";
-        default:
-            return "#a3ff00";
-    }
+    var overlay = {
+        "Earthquakes": earthquakes,
+        "Fault Lines": tectonicplates,
     }
 
-function getRadius(magnitude) {
-    if (magnitude === 0) {
-        return 1;
-    }
-
-      return magnitude * 5;
-    }
+    L.control.layers(basemaps, overlay).addTo(myMap)
 
 
-    L.geoJson(data, {
-    pointToLayer: function (feature, latlong) {
-        return L.circleMarker(latlong);
-    },
 
-    style: styleInfo,
-
-
-    onEachFeature: function (feature, layer) {
-
-        layer.bindPopup("Earthquake Magnitude: " + feature.properties.mag + "<br>Earthquake Location:<br>" + feature.properties.place);
-    }
-    }).addTo(myMap);
+    function getColor(magnitude) {
+        switch (true) {
+            case magnitude > 5:
+                return "#ff0000";
+            case magnitude > 4:
+                return "#ff7300";
+            case magnitude > 3:
+                return "#fbff00";
+            case magnitude > 2:
+                return "#eecc00";
+            case magnitude > 1:
+                return "#d4ee00";
+            default:
+                return "#a3ff00";
+        }
+        }
 
     var legend = L.control({ position: 'bottomright' });
 
-    legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend'),
+    legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'info legend'),
         grades = [0, 1, 2, 3, 4, 5];
-        div.innerHTML = 'Eathquake<br>Magnitude<br><hr>'
+
+        div.innerHTML += 'Eathquake<br>Magnitude<br><hr>'
 
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
@@ -87,4 +85,95 @@ function getRadius(magnitude) {
     return div;
     };
     legend.addTo(myMap);
+}
+
+var remainingCalls = 2;
+var earthquakesLayer = []
+var faultlinelLayer = []
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+
+d3.json(geoData, function (data) {
+
+    function styleInfo(feature) {
+        return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+        };
+    }
+
+
+    function getColor(magnitude) {
+        switch (true) {
+            case magnitude > 5:
+                return "#ff0000";
+            case magnitude > 4:
+                return "#ff7300";
+            case magnitude > 3:
+                return "#fbff00";
+            case magnitude > 2:
+                return "#eecc00";
+            case magnitude > 1:
+                return "#d4ee00";
+            default:
+                return "#a3ff00";
+        }
+        }
+
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+        return 1;
+    }
+
+        return magnitude * 5;
+    }
+
+    var earthquakes = new L.LayerGroup()
+    L.geoJson(data, {
+
+    pointToLayer: function (feature, latlong) {
+        return L.circleMarker(latlong);
+    },
+
+    style: styleInfo,
+    
+    onEachFeature: function (feature, layer) {
+
+    layer.bindPopup("Earthquake Magnitude: " + feature.properties.mag + "<br>Earthquake Location:<br>" + feature.properties.place);
+        }
+    }).addTo(earthquakes);
+
+    earthquakesLayer = earthquakes
+    --remainingCalls;
+    //console.log(`Fetched earthquake data. Remaining calls: ${remainingCalls}`)
+
+    if (remainingCalls === 0) {
+    createMap(earthquakesLayer, faultlinelLayer)
+    }
+
 });
+
+
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json", function (data) {
+
+    var faultlines = new L.LayerGroup()
+
+    L.geoJson(data, {
+        color: "orange",
+        weight: 2,
+    }).addTo(faultlines);
+
+    faultlinelLayer = faultlines
+
+    --remainingCalls;
+    //console.log(`Fetched faultline data. Remaining calls: ${remainingCalls}`)
+    if (remainingCalls === 0) {
+        createMap(earthquakesLayer, faultlinelLayer)
+    }
+});
+
